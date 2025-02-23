e22a6f387af25e014fe79babfe0143cfb9eea90b
Eran Hsu <eran0926@gmail.com>
2025-02-23 13:26:35 +0800

    feat: integrate responsive framework for adaptive UI layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:responsive_framework/responsive_framework.dart';
+
 import 'package:flutter/material.dart';
 import 'package:viewer/router.dart';
 
@@ -11,9 +13,17 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      routerConfig: router,
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        routerConfig: router,
+        builder: (context, child) => ResponsiveBreakpoints.builder(
+              child: child!,
+              breakpoints: [
+                const Breakpoint(start: 0, end: 450, name: MOBILE),
+                const Breakpoint(start: 451, end: 800, name: TABLET),
+                const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+                const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+              ],
+            ));
   }
 }
