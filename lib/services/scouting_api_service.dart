2cfd65a4c4bd8dfdf2fbaa726036d9d914598ecf
Eran Hsu <eran0926@gmail.com>
2025-02-28 21:22:23 +0800

    feat: add fetchObjectiveData method to api service

--- lib/services/scouting_api_service.dart
+++ lib/services/scouting_api_service.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 
 import 'package:viewer/utils/logger.dart';
 
 class ScoutingApiService {
-  final String _baseUrl = 'http://localhost:8000';
+  final Uri _baseUrl = Uri.parse('http://localhost:8000');
 
   Future<List<String>> fetchTeams() async {
     try {
-      final response = await http.get(Uri.parse('$_baseUrl/team_list'));
+      final response = await http.get(_baseUrl.replace(path: '/team_list'));
       logger.i('Response code: ${response.statusCode}');
       if (response.statusCode == 200) {
         final List<dynamic> data = jsonDecode(response.body);
@@ -21,6 +22,29 @@ class ScoutingApiService {
       // print('Error: $e');
       logger.e('Error: $e');
       return [];
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchObjectiveData(String teamNumber) async {
+    try {
+      final response = await http.get(_baseUrl.replace(
+          path: '/objective/result',
+          queryParameters: {'team_number': teamNumber}));
+      logger.i('Response code: ${response.statusCode}');
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        logger.i('Objective data: $data');
+        return data;
+      }
+      if (response.statusCode == 404) {
+        return {};
+      } else {
+        throw Exception('Failed to load objective data');
+      }
+    } catch (e) {
+      // print('Error: $e');
+      logger.e('Error: $e');
+      return {};
     }
   }
 }
