b1e1b76b46cb03e7760da150f1cffe9fc51c5cbf
Eran Hsu <eran0926@gmail.com>
2025-03-02 18:40:11 +0800

    feat: add chart color set constants for consistent styling

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const chartColorSet = [
+  Color(0xff4fc3f7),
+  Color(0xff9575cd),
+  Color(0xff4db6ac),
+  Color(0xffe57373),
+  Color(0xffffd54f),
+  Color(0xffa1887f),
+  Color(0xff4db6ac),
+];
