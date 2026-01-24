a2e3555f23d0b9fd7d0cc229fd1e328b5a2d4cb2
eran0926 <eran0926@users.noreply.github.com>
2026-01-24 08:25:44 +0000

    Deploying to gh-pages from @ eran0926/frc6998-scouting-dashboard-2025@507659031982698de975f4853e025f231742c69c 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,8 +31,8 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "33ea3ff81dd816e6348236e839d12dbe",
-"version.json": "06ae342215125fb557ddff78795457aa",
+"flutter_bootstrap.js": "7fc223611a54dd102312b79cdaa4dc50",
+"version.json": "a6be7a48ec49fbe34e0a794758f28333",
 "main.dart.js": "03d99614c1a55a3cc032278274a94572"};
 // The application shell files that are downloaded before a service worker can
 // start.
